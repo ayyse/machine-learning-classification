6915ce59c838e3da260897bae40b0b897ee24df7
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2021-06-13 19:18:33 +0300

    classification algorithms added

--- final-project-classification.py
+++ final-project-classification.py
@@ -13,6 +13,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix  
 from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+
 
 # ********************* Read Data *******************************************
 df = pd.read_excel('C:/Users/Acer/Desktop/metadata_train.xlsx', header = None)
@@ -74,29 +82,109 @@ notfaulttrain = extractFeature(preprocessing_data,250,10,"0")
 # fault train
 faulttrain = extractFeature(preprocessing_data,250,10,"1")
 
-train = pd.concat([notfaulttrain, faulttrain])
+merged_train = pd.concat([notfaulttrain, faulttrain])
 
 # not fault test
 notfaulttest = extractFeature(preprocessing_data,250,10,"0")
 # fault test
 faulttest = extractFeature(preprocessing_data,250,10,"1")
 
-test = pd.concat([notfaulttest, faulttest])
+merged_test = pd.concat([notfaulttest, faulttest])
 # ******************************************************************************
 
 # ************************ Decision Tree Algorithm *****************************
-classifier = DecisionTreeClassifier()
-classifier.fit(X_train, y_train)
+classifier_dt = DecisionTreeClassifier()
+classifier_dt.fit(X_train, y_train)
 
-y_pred = classifier.predict(X_test)
+y_pred = classifier_dt.predict(X_test)
 
 # number of correct and incorrect predictions
-cm = confusion_matrix(y_test, y_pred)  
+cm_dt = confusion_matrix(y_test, y_pred)  
 
-print('Accuracy score on train data:', accuracy_score(y_true = y_train, y_pred = classifier.predict(X_train)))
-print('Accuracy score on test data:', accuracy_score(y_true = y_test, y_pred = classifier.predict(X_test)))
+print('(Decision Tree) Accuracy score on train data:', accuracy_score(y_true = y_train, y_pred = classifier_dt.predict(X_train)))
+# print('Accuracy score on test data:', accuracy_score(y_true = y_test, y_pred = classifier.predict(X_test)))
 # ******************************************************************************
 
+# ******************************* KNN Algorithm ******************************** 
+classifier_knn = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2 )  
+classifier_knn.fit(X_train, y_train) 
+
+y_pred = classifier_knn.predict(X_test)
+
+cm_knn = confusion_matrix(y_test, y_pred)  
+
+print('(KNN) Accuracy score on train data:', accuracy_score(y_true = y_train, y_pred = classifier_knn.predict(X_train)))
+# ******************************************************************************
+
+# ************************* Logistic Regression Algorithm **********************
+classifier_lr = LogisticRegression(random_state = 0)  
+classifier_lr.fit(X_train, y_train) 
+
+y_pred = classifier_lr.predict(X_test)
+
+cm_lr = confusion_matrix(y_test, y_pred)  
+
+print('(Logistic Regression) Accuracy score on train data:', accuracy_score(y_true = y_train, y_pred = classifier_lr.predict(X_train)))
+# ******************************************************************************
+
+# ****************************** Naive Bayes Algorithm *************************
+classifier_nb = GaussianNB()  
+classifier_nb.fit(X_train, y_train) 
+
+y_pred = classifier_nb.predict(X_test)
+
+cm_nb = confusion_matrix(y_test, y_pred)  
+
+print('(Naive Bayes) Accuracy score on train data:', accuracy_score(y_true = y_train, y_pred = classifier_nb.predict(X_train)))
+# ******************************************************************************
+
+
+# ************************* Random Forest Algorithm ****************************
+classifier_rf = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0) 
+classifier_rf.fit(X_train, y_train) 
+
+y_pred = classifier_rf.predict(X_test)
+
+cm_rf = confusion_matrix(y_test, y_pred)  
+
+print('(Random Forest) Accuracy score on train data:', accuracy_score(y_true = y_train, y_pred = classifier_rf.predict(X_train)))
+# ******************************************************************************
+
+
+# ************************* AdaBoost Algorithm ****************************
+classifier_ab = AdaBoostClassifier(n_estimators=50, learning_rate=1)
+classifier_ab.fit(X_train, y_train) 
+
+y_pred = classifier_ab.predict(X_test)
+
+cm_ab = confusion_matrix(y_test, y_pred)  
+
+print('(AdaBoost) Accuracy score on train data:', accuracy_score(y_true = y_train, y_pred = classifier_ab.predict(X_train)))
+# ******************************************************************************
+
+
+# ****************************** SVC Algorithm *********************************
+classifier_sv = SVC(kernel='linear', random_state = 0)
+classifier_sv.fit(X_train, y_train) 
+
+y_pred = classifier_sv.predict(X_test)
+
+cm_sv = confusion_matrix(y_test, y_pred)  
+
+print('(SVC) Accuracy score on train data:', accuracy_score(y_true = y_train, y_pred = classifier_sv.predict(X_train)))
+# ******************************************************************************
+
+
+# ***************** Multi-Layer Perceptron (MLP/ANN) Algorithm *****************
+classifier_mlp = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+classifier_mlp.fit(X_train, y_train) 
+
+y_pred = classifier_mlp.predict(X_test)
+
+cm_mlp = confusion_matrix(y_test, y_pred)  
+
+print('(MLP) Accuracy score on train data:', accuracy_score(y_true = y_train, y_pred = classifier_mlp.predict(X_train)))
+# ******************************************************************************
 
 
 
